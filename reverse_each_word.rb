264c0a1f0b1508d86b834d245255ae2d3f8bbaa2
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-08 17:55:48 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,20 @@
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(" ")
+  reversed_array = []
+  sentence_array.collect do |word| 
+      reversed_array << word.reverse 
+  end
+  reversed_array.join(" ")
+end
+
+
+
+#def reverse_each_word(sentence)
+ # sentence_array = sentence.split(" ")
+#  reversed_array = []
+#  sentence_array.each do |word| 
+    #reversed_array word.reverse 
+    reversed_array << word.reverse 
+ # end
+ # reversed_array.join(" ")
+#end
